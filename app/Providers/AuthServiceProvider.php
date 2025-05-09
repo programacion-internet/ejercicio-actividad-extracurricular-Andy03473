5ac4d64f0a0c39c9bc89d00507ea61ae75c3255f
Andy03473 <andrea.ortega4657@alumnos.udg.mx>
2025-05-09 00:06:43 -0600

    Email y policy (archivos)

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Archivo;
+use App\Policies\ArchivoPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        Archivo::class => ArchivoPolicy::class,
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
+
